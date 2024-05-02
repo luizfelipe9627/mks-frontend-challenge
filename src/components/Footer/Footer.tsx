b2220e30179001144998a361581da519add8af01
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-05-02 10:45:25 -0300

    feat: criado componente Footer.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./FooterStyles";
+
+const Footer = () => {
+  return (
+    <S.Footer>
+      <S.Container>
+        <p>MKS sistemas © Todos os direitos reservados</p>
+      </S.Container>
+    </S.Footer>
+  );
+};
+
+export default Footer;
