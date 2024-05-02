620627f2dbb64ce6554accb233446777f7e199eb
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-05-02 10:46:02 -0300

    feat: criado componente Logo.

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./LogoStyles";
+
+const Logo = () => {
+  return (
+    <S.LogoTitle>
+      MKS <S.LogoSubtitle>Sistemas</S.LogoSubtitle>
+    </S.LogoTitle>
+  );
+};
+
+export default Logo;
