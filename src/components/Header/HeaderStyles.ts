8d9ac7c08b764bbeca1f5ef3bd7ecd4c6a0d7a23
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-05-02 10:45:50 -0300

    feat: criado estilo do componente Header.

---
+++ src/components/Header/HeaderStyles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  background-color: #0f52ba;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+  padding: 1.875rem 0;
+  max-width: 90vw;
+`;
