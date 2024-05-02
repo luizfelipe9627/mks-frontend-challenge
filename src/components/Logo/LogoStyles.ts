f29611bc54c7966dcbd9cce4fa22f768877b8fac
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-05-02 10:45:57 -0300

    feat: criado estilo do componente Logo.

---
+++ src/components/Logo/LogoStyles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const LogoTitle = styled.h1`
+  font-size: 2.5rem;
+  color: #fff;
+  font-weight: 600;
+  line-height: 1.2;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+`;
+
+export const LogoSubtitle = styled.span`
+  font-size: 1.25rem;
+  color: #fff;
+  font-weight: 200;
+`;
