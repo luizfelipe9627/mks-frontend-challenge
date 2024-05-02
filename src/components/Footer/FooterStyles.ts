cd5a137416f16128040edf82450892463440086f
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-05-02 10:45:32 -0300

    feat: criado estilo do componente Footer.

---
+++ src/components/Footer/FooterStyles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+  background-color: #eeeeee;
+  color: #000;
+  text-align: center;
+`;
+
+export const Container = styled.div`
+  max-width: 90vw;
+  padding: 1.875rem;
+`;
